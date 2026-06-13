fb6e8889c5bce3191898bebf7ab801f03af9ed94
Khalid Alli-B <khalidallibalogun08@gmail.com>
2026-06-12 23:10:24 -0500

    connected reducer to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { useReducer } from "react";
 import "./css/index.css";
 import TaskForm from "./components/TaskForm";
+import { taskReducer } from "./reducers/taskReducer";
 
 function App() {
+  const [tasks, dispatch] = useReducer(taskReducer, []);
+
   return (
     <div className="container">
       <h1>Task Tracker</h1>
-      <TaskForm />
+
+      <TaskForm dispatch={dispatch} />
+
+      <p>Total Tasks: {tasks.length}</p>
     </div>
   );
 }
