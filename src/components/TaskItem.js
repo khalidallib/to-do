28223561d456ee42152c891480626b28d499389a
Khalid Alli-B <khalidallibalogun08@gmail.com>
2026-06-12 23:58:13 -0500

    final push

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,15 +1,72 @@
+import { useState } from "react";
+
 function TaskItem({ task, dispatch }) {
+  const [editing, setEditing] = useState(false);
+  const [newText, setNewText] = useState(task.text);
+
+  function deleteTask() {
+    dispatch({
+      type: "DELETE_TASK",
+      id: task.id
+    });
+  }
+
+  function completeTask() {
+    dispatch({
+      type: "COMPLETE_TASK",
+      id: task.id
+    });
+  }
+
+  function saveTask() {
+    if (newText.trim() === "") {
+      alert("Task cannot be empty.");
+      return;
+    }
+
+    dispatch({
+      type: "UPDATE_TASK",
+      id: task.id,
+      newText: newText
+    });
+
+    setEditing(false);
+  }
+
   return (
-    <div className="task">
-      <h3>{task.text}</h3>
-      <p>{task.date}</p>
-      <button onClick={() =>
-          dispatch({
-            type: "DELETE_TASK",
-            id: task.id
-          })
-        }
-      > Delete </button>
+    <div className={task.completed ? "task completed" : "task"}>
+      {editing ? (
+        <>
+          <input
+            type="text"
+            value={newText}
+            onChange={(event) =>
+              setNewText(event.target.value)
+            }
+          />
+
+          <button onClick={saveTask}>
+            Save
+          </button>
+        </>
+      ) : (
+        <>
+          <h3>{task.text}</h3>
+          <p>{task.date}</p>
+        </>
+      )}
+
+      <button onClick={completeTask}>
+        Complete
+      </button>
+
+      <button onClick={() => setEditing(true)}>
+        Edit
+      </button>
+
+      <button onClick={deleteTask}>
+        Delete
+      </button>
     </div>
   );
 }
