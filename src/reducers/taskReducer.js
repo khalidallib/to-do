eaabea8ddc2a1b241acc7d2f0a7fe3945aa5da2e
Khalid Alli-B <khalidallibalogun08@gmail.com>
2026-06-12 23:54:44 -0500

    added css and update task

--- src/reducers/taskReducer.js
+++ src/reducers/taskReducer.js
@@ -16,6 +16,27 @@ export function taskReducer(tasks, action) {
         (task) => task.id !== action.id
       );
 
+    case "COMPLETE_TASK":
+      return tasks.map((task) =>
+        task.id === action.id
+          ? {
+              ...task,
+              completed: !task.completed
+            }
+          : task
+      );
+
+    case "UPDATE_TASK":
+      return tasks.map((task) =>
+        task.id === action.id
+          ? {
+              ...task,
+              text: action.newText,
+              date: new Date().toLocaleString()
+            }
+          : task
+      );
+
     default:
       return tasks;
   }
